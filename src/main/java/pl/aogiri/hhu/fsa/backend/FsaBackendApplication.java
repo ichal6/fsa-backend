cf14b7ef72bc3f15145bade45a29183aa2685b92
KanekiKen <vkanekisenpai@gmail.com>
2022-11-27 12:16:31 +0100

    FSA-11 Prepare base for project in repo for backend

---
+++ src/main/java/pl/aogiri/hhu/fsa/backend/FsaBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.aogiri.hhu.fsa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsaBackendApplication.class, args);
+    }
+
+}
