407ea946024ec9cff12e7232eadcaf9030a54bdb
KanekiKen <vkanekisenpai@gmail.com>
2022-11-27 16:47:32 +0100

    FSA-11 Prepare docker image, application dev profile and base integration test

---
+++ src/test/java/pl/aogiri/hhu/fsa/backend/common/IntegrationTest.java
@@ -0,0 +1,25 @@
+package pl.aogiri.hhu.fsa.backend.common;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@Profile("integration")
+public abstract class IntegrationTest {
+    @Container
+    private static final PostgreSQLContainer container = new PostgreSQLContainer("postgres:15.1");
+
+    @DynamicPropertySource
+    static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
+
+}
