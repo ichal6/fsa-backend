8dcd5017b843e3877f9f390d8b2c3b50edbd52ef
SamuelDidoszak <samueldidoszak1@gmail.com>
2023-01-13 12:52:29 +0100

    FSA-108 Prepared endpoint for queried showtime fetching

--- src/test/java/pl/aogiri/hhu/fsa/backend/showtime/application/ShowtimeServiceTest.java
+++ src/test/java/pl/aogiri/hhu/fsa/backend/showtime/application/ShowtimeServiceTest.java
@@ -5,13 +5,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import pl.aogiri.hhu.fsa.backend.showtime.application.dto.ShowtimeCriteriaRequest;
 import pl.aogiri.hhu.fsa.backend.showtime.application.mapper.ShowtimeDtoFixture;
 import pl.aogiri.hhu.fsa.backend.showtime.application.mapper.ShowtimeEntityFixture;
 import pl.aogiri.hhu.fsa.backend.showtime.domain.repository.ShowtimeRepository;
 import pl.aogiri.hhu.fsa.backend.showtime.exception.ShowtimeNotFoundException;
 
-import java.util.List;
 import java.util.Optional;
 
 import static java.lang.String.format;
@@ -57,26 +55,5 @@ public class ShowtimeServiceTest {
 
         //then
         assertThat(exception.getMessage()).isEqualTo(format("Showtime with id %d not found", showtimeId));
-    }
-
-    @Test
-    void shouldReturnAllShowtime() {
-        //given
-        final var showtimes = List.of(
-                ShowtimeEntityFixture.showtimeTheIncrediblesInCinemaCityBonarka(),
-                ShowtimeEntityFixture.showtimeAvatarTheWayOfWaterInCinemaCityBonarka());
-        final var criteria = new ShowtimeCriteriaRequest(null, null, null, null, null, null, null);
-
-        given(showtimeRepository.findAll()).willReturn(showtimes);
-
-        //when
-        final var actualShowtime = showtimeService.getShowtimes(criteria);
-
-        //then
-        assertThat(actualShowtime)
-                .hasSize(2)
-                .contains(
-                        ShowtimeDtoFixture.showtimeTheIncrediblesInCinemaCityBonarka(),
-                        ShowtimeDtoFixture.showtimeAvatarTheWayOfWaterInCinemaCityBonarka());
     }
 }
